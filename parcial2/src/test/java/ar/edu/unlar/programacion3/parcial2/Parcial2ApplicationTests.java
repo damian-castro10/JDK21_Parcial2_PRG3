7b02f0144f4061d67d9e18ac65d90f09c0f67310
damian-castro10 <emilianocastro160905@gmail.com>
2026-06-17 22:40:52 -0300

    inicio

---
+++ parcial2/src/test/java/ar/edu/unlar/programacion3/parcial2/Parcial2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unlar.programacion3.parcial2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Parcial2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
